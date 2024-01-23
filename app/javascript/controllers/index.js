8820be25950e5f0b25f18a0ac173bef78fd19e9f
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-01-23 11:10:32 -0300

    - removing lock and compiled files from git - adjust bootstrap to importmap - add importmap to project - fixed font-awesome to scss files - add cable ready gem - application html with importmap - ajust directories of js and css - (adjust sorry) -- need to fix when some error occurs in the process because the turbostream showup with error

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,8 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "controllers/application"
 
-import DropzoneController from "./dropzone_controller"
-application.register("dropzone", DropzoneController)
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
